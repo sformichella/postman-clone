1c5a66fbc9747a42dca7de98e1d8353cc710f7ff
sformichella <sformichella@gmail.com>
2021-02-07 14:22:46 -0800

    Go component

--- src/components/presentation/Go/Go.jsx
+++ src/components/presentation/Go/Go.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-export default function Go() {
+export default function Go({ queryOnClick }) {
   return (
     <>
-
+      <button onClick={ queryOnClick }>Query!</button>
     </>
   );
 }
+
+Go.propTypes = {
+  queryOnClick: PropTypes.func.isRequired
+};
