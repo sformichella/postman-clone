d26151c82b609f952ee06e83bbbeab87a7cf5144
sformichella <sformichella@gmail.com>
2021-02-07 14:49:57 -0800

    Request container scaffolding

---
+++ src/components/containers/Request.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Url from "../presentation/Url/Url";
+import HTTPVerb from "../presentation/HTTPVerb/HTTPVerb";
+import Go from "../presentation/Go/Go";
+import Body from "../presentation/Body/Body";
+
+export default class Request extends Component {
+  render() {
+    return (
+      <>
+        
+      </>
+    );
+  }
+}
