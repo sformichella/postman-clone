c9b57c31644f3b2860311e1c5783e044147a2859
sformichella <sformichella@gmail.com>
2021-02-07 15:05:30 -0800

    add Request to App

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import Request from "../containers/Request/Request";
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Request />
+  );
 }
