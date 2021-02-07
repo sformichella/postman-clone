1d1c54d967d46cdef7c5f2dc84000ab97267436b
sformichella <sformichella@gmail.com>
2021-02-07 15:05:43 -0800

    forgot to save

--- src/components/containers/Request/Request.jsx
+++ src/components/containers/Request/Request.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Url from "../presentation/Url/Url";
-import HTTPVerb from "../presentation/HTTPVerb/HTTPVerb";
-import Go from "../presentation/Go/Go";
-import Body from "../presentation/Body/Body";
+import Url from "../../presentation/Url/Url";
+import HTTPVerb from "../../presentation/HTTPVerb/HTTPVerb";
+import Go from "../../presentation/Go/Go";
+import Body from "../../presentation/Body/Body";
 
 export default class Request extends Component {
 
